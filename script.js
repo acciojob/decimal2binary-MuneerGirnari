7e0f21805a46a9d64bc649e9190943e283597a5d
Muneer Girnari <muneeragirnari@gmail.com>
2023-08-23 11:40:04 +0530

    solved script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let str = "";
+  while(num){
+    let remainder = num%2;
+    str = remainder + str;
+    num = Math.floor(num/2);    
+  }
+  return str;
 }
 
 window.decimalToBinary = decimalToBinary;
